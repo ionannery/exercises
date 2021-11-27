d4976c7ca5168fb6943f95f666b2ae9fb3fb4d90
ionannery <ionan16@hotmail.com>
2021-11-27 00:44:39 -0300

    att exercise 4

--- fundamentos/bloco-04-JavaScript-Primeiros-Passos/script.js
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/script.js
@@ -35,7 +35,7 @@ let message = "";
 if( currentHour >= 22) {
     message = "Não deveríamos comer nada, é hora de dormir";
 } else if (currentHour >= 18 && currentHour < 22) {
-    message= "Rango da noite, vamos jantar :D";
+    message = "Rango da noite, vamos jantar :D";
 } else if (currentHour >= 14 && currentHour < 18) {
     message = "Vamos fazer um bolo pro café da tarde?";
 } else if (currentHour >= 11 && currentHour < 14) {
@@ -43,6 +43,8 @@ if( currentHour >= 22) {
 } else if (currentHour > 4 && currentHour < 11) {
     message = "Hmmm, cheiro de café recém passado";
 }
+
+console.log(message);
 
 // Quinto exercício
 
