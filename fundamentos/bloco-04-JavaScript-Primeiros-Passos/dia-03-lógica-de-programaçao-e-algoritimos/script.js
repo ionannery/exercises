0ee678622cca65aa8da3fae2dfcc41e6f4b0a137
ionannery <ionan16@hotmail.com>
2021-11-30 19:05:04 -0300

    adiciona exercicios bloco 3 dia 4

---
+++ fundamentos/bloco-04-JavaScript-Primeiros-Passos/dia-03-lógica-de-programaçao-e-algoritimos/script.js
@@ -0,0 +1,101 @@
+// Exercise 01
+
+let n = 5;
+let simbolo = "*";
+
+for (let index = 1; index <= n; index++) {
+    let linha = "";
+    for (let coluna = 1; coluna <= n; coluna++) {
+        linha = linha + "*";
+    }
+    console.log(linha);
+}
+
+// Exercise 02
+
+let size = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex <= size; lineIndex += 1) {
+  console.log(inputLine);
+  inputLine = inputLine + symbol;
+};
+
+// Exercise 03 
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+let inputPosition = n;
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex < inputPosition) {
+      inputLine = inputLine + ' ';
+    } else {
+      inputLine = inputLine + symbol;
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  inputPosition -= 1;
+};
+
+// Exercise 04
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+let midOfMatrix = (n + 1) / 2;
+let controlLeft = midOfMatrix;
+let controlRight = midOfMatrix;
+
+for (let lineIndex = 0; lineIndex <= midOfMatrix; lineIndex += 1) {
+  for (let columnIndex = 0; columnIndex <= n; columnIndex += 1) {
+    if (columnIndex > controlLeft && columnIndex < controlRight) {
+      inputLine = inputLine + symbol;
+    } else {
+      inputLine = inputLine + ' ';
+    }
+  }
+  console.log(inputLine);
+  inputLine = '';
+  controlRight += 1;
+  controlLeft -= 1
+};
+
+// Exercise 05
+
+let n = 7;
+let middle = (n + 1) / 2;
+let controlLeft = middle;
+let controlRight = middle;
+let symbol = '*';
+for (let line = 1; line <= middle; line += 1) {
+  let outputLine = '';
+  for (let col = 1; col <= n; col += 1) {
+    if (col == controlLeft || col == controlRight || line == middle) {
+      outputLine += symbol;
+    } else {
+      outputLine += ' ';
+    }
+  }
+  controlLeft -= 1;
+  controlRight += 1;
+  console.log(outputLine);
+}
+
+// Exercise 06
+
+
+let divisors = 1;
+let numberToCheck = 83;
+
+for (let number = 2; number <= numberToCheck; number += 1) {
+  if (numberToCheck % number === 0) divisors += 1;
+}
+
+if (divisors === 2) console.log(numberToCheck + ' é primo');
+else console.log(numberToCheck + ' não é primo');
