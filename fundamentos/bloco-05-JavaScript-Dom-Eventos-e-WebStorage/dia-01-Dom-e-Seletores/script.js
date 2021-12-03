10a291062bf7107460858c60f89099f7f629226c
ionannery <ionan16@hotmail.com>
2021-12-03 20:05:18 -0300

    exercise dia 1 bloco 5

---
+++ fundamentos/bloco-05-JavaScript-Dom-Eventos-e-WebStorage/dia-01-Dom-e-Seletores/script.js
@@ -0,0 +1,13 @@
+document.getElementById("header-container").style.background = ('green')
+
+document.querySelector(".emergency-tasks h3").style.background = ('red');
+
+document.querySelector(".emergency-tasks").style.background = ('#2345');
+
+document.querySelector(".no-emergency-tasks h3").style.background = ('red');
+
+document.querySelector(".no-emergency-tasks").style.background = ('#2345');
+
+document.getElementById("footer-container").style.background = ('red');
+
+document.getElementById("container").style.background = ('black');
