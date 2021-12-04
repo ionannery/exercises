21a7d26a5ae8dc0739941bf687766b523aadfc91
ionannery <ionan16@hotmail.com>
2021-12-04 20:02:55 -0300

    adiciona exercicios DOM

---
+++ fundamentos/bloco-05-JavaScript-Dom-Eventos-e-WebStorage/dia-01-Dom-e-Seletores/script1.js
@@ -0,0 +1,7 @@
+document.getElementById("page-title").innerText = "Interestelar";
+
+document.getElementsByClassName("paragraph")[0].innerText = "O melhor filme do MUNDO";
+
+document.getElementsByTagName("h4")[0].style.color = "red"
+
+document.getElementsByClassName("paragraph")[0].style.backgroundColor = "#2345";
